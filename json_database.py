35369d957027d8fdd67a4c94f16c15e509ea34cc
devinyems156 <devinyems156@gmail.com>
2023-07-13 19:06:04 +0300

    first commit

---
+++ json_database.py
@@ -0,0 +1,52 @@
+import json
+import os
+
+
+def readjson(name, home_folder=''):
+    try:
+        with open(home_folder + name, 'r') as f:
+            value = json.load(f)
+            return value
+    except json.decoder.JSONDecodeError:
+        print('JSONDecodeError: file data is too short or file is empty')
+    except FileNotFoundError:
+        print('FileNotFoundError: no such file')
+
+
+def writejson(name, value, home_folder='', abs=False):
+    if abs:
+        self_file = __file__
+        own_path = ''
+        for i in self_file.split(sep='\\')[:-1]:
+            own_path = own_path + i + '/'
+        print(own_path)
+        home_folder = own_path + home_folder
+    print(home_folder)
+    if not home_folder == '':
+        os.makedirs(name=home_folder, exist_ok=True),
+    with open(home_folder + name, 'w') as f:
+        json.dump(value, f, indent=4)
+
+
+default = {
+    'token': 'MTEyOTA0NDg5NDg3MTE5MTY3Mg.GMv_ao.Da3oP5TIMvpP-UazX6SCodyKx4rSYJjy0KNk7E',
+    'time': 60,
+    'ip': 'game1.falixserver.net:63546',
+    'channel_id': 1129045123955703871,
+    'embed_color': 0x75a4c3,
+    'message_id':  None
+}
+
+
+def getdata():
+    info = readjson('info.json')
+    if not info:
+        writejson('info.json', default)
+        info = readjson('info.json')
+    return info
+
+
+def senddata(data):
+    writejson('info.json', data)
+
+
---
+++ main.py
@@ -0,0 +1,49 @@
+from mcstatus import JavaServer
+import discord
+import asyncio
+import datetime
+import json_database
+
+bot = discord.Bot()
+data = json_database.getdata()
+
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+    await update_status()
+
+
+async def update_status():
+    while True:
+        print('cycle')
+        server = JavaServer.lookup(data['ip'])
+        try:
+            status = server.status()
+            print(status)
+            if status.players.max == 0:
+                online = False
+            else:
+                online = True
+                desc = f'Онлайн | {status.players.online}/{status.players.max} игроков'
+        except Exception as e:
+            online = False
+            print('exception', e)
+        embed = discord.Embed(title=data['ip'], description=desc if online else 'Офлайн', color=data['embed_color'])
+        ts = datetime.datetime.now().time().strftime('%H:%M')
+        embed.set_footer(text=' Обновленоᅠ•ᅠCегодня в ' + ts)
+        channel = bot.get_channel(data['channel_id'])
+        if not data['message_id']:
+            message = await channel.send(embed=embed)
+            data['message_id'] = message.id
+            json_database.senddata(data)
+        else:
+            await channel.get_partial_message(data['message_id']).edit(embed=embed)
+        await asyncio.sleep(data['time'])
+
+
+def startup():
+    return bot.run(token=data['token'])
+
+
+startup()
